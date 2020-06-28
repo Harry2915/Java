e2b10541099bc5b7e96431c607ee3db677b04845
Harry2915 <harishhibare2915@gmal.com>
2020-06-28 22:32:00 +0530

    fixed bugs

--- primeno/PrimeNo.java
+++ primeno/PrimeNo.java
@@ -1,23 +1,35 @@
 package harish.hibare.javaday;
 
 public class PrimeNo {
-    static int  prime = 0,sum=0;
-    public static void main(String[] args) {
-        for (int i=1; i <= 100; i++) {
-            System.out.println("Checking number:: "+i);
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0)
-                    prime++;
-            }
-            if (prime == 2){
-                System.out.println(+i+" ::is prime number");
-                System.out.println("Old Count:: "+sum);
-                sum=sum+i;
-                System.out.println("New Count::" +sum);
-            }
-            else System.out.println(+i+" ::is not prime number");
-            prime = 0;
+    public static boolean isPrime(int num) {
+        if (num == 2 || num == 3) {
+            System.out.println(+num+" ::Is prime number\n");
+            return true;
         }
-        System.out.println("\n\nSUM OF PRIME NUMBER BETWEEN 1-100:: "+sum+"\n");
+        if (num % 2 == 0 || num % 3 == 0) {
+            System.out.println(+num+" ::Is not prime number\n");
+            return false;
+        }
+        for (int i = 3; i < Math.sqrt(num); i += 2) {
+            if (num % Math.sqrt(num) == 0 || num % i == 0) {
+                System.out.println(+num+" ::Is not prime number\n");
+                return false;
+            }
+        }
+        System.out.println(+num+" ::Is prime number\n");
+        return true;
+    }
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i = 2; i <= 100; i++)
+        {
+            if (isPrime(i))
+                sum=sum+i;
+        }
+        System.out.println("\nSum of prime number's between 1-100:: "+sum);
     }
 }
+
+
+
+
